bbddc2a11c3a7b1426b0551f34ea2023c3d949f2
João Gabriel <joaogabriel.trindade04@gmail.com>
2026-03-24 23:58:07 -0300

    fixed: corrigindo nós se sobrepondo e centralizando árvore no painel

--- src/PainelArvore.java
+++ src/PainelArvore.java
@@ -14,19 +14,24 @@ public class PainelArvore extends JPanel{
         g.drawOval(x, y, 30, 30);
         g.drawString(String.valueOf(no.valor), x + 10, y + 20);
 
+        int novoDist = Math.max(dist / 2, 40); //evita que o dist fica muito pequeno
+
         if (no.esquerda != null) {
             g.drawLine(x + 15, y + 30, x - dist + 15, y + 80);
-            desenhar(g, no.esquerda, x - dist, y + 80, dist / 2);
+            desenhar(g, no.esquerda, x - dist, y + 80, novoDist);
         }
         if (no.direita != null) {
             g.drawLine(x + 15, y + 30, x + dist, y + 80);
-            desenhar(g, no.direita, x + dist, y + 80, dist / 2);
+            desenhar(g, no.direita, x + dist, y + 80, novoDist);
         }
     }
 
 
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
-        desenhar(g, arvore.raiz, 300, 30, 150);
+
+        int xCentro = getWidth() / 2; // Centraliza no painel
+
+        desenhar(g, arvore.raiz, xCentro, 30, 150);
     }
 }
